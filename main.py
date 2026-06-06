0e625be1415ce2e09098720180c165c613c50454
videttapp <videttapp@hotmail.com>
2026-06-06 14:55:11 +0100

    Set up templates

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from flask!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
